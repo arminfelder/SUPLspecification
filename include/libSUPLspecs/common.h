b0f2405275c749339867187b3f92537a0187bead
Armin Felder <Armin.Felder@gmail.com>
2021-06-27 16:24:10 +0200

    initial

---
+++ include/libSUPLspecs/common.h
@@ -0,0 +1,19 @@
+//
+// Created by armin on 27.06.21.
+//
+
+#ifndef LIBSUPLSPEC_COMMON_H
+#define LIBSUPLSPEC_COMMON_H
+
+/* Dependencies */
+typedef enum modeSpecificInfo_PR {
+    modeSpecificInfo_PR_NOTHING,	/* No components present */
+    modeSpecificInfo_PR_fdd,
+    modeSpecificInfo_PR_tdd,
+    /* Extensions may appear below */
+
+} modeSpecificInfo_PR;
+
+
+
+#endif //LIBSUPLSPEC_COMMON_H
